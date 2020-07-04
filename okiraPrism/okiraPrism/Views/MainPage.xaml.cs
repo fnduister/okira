086e593a2b3bcb6475b61d9a8d8b3cac0a891eed
fabrice <fnduister@gmail.com>
2020-07-03 23:00:11 -0400

    first real commit

---
+++ okiraPrism/okiraPrism/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace okiraPrism.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            label.Text = slider.Value.ToString();
+        }
+
+        private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = e.NewValue.ToString();
+        }
+    }
+}
