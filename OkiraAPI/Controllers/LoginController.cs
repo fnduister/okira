6f7078797d4cf9bb30da5a67aa295ecb54f2b10e
Mamoudou Sacko <msacko99@yahoo.com>
2020-07-06 23:15:40 -0400

    add login api

---
+++ OkiraAPI/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using OkiraAPI.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OkiraAPI.Controllers
+{
+    [RoutePrefix("api")]
+    public class LoginController : ApiController
+    {
+        [Route("login/{username}")]
+        [AllowAuthenticatedUsers(Username = "username")]
+        public IHttpActionResult Login(string username)
+        {
+            //En attente 
+            return Ok();
+        }
+    }
+}
---
+++ OkiraAPI/Tools/AllowAuthenticatedUsers.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OkiraAPI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace OkiraAPI.Tools
+{
+    public class AllowAuthenticatedUsers : ActionFilterAttribute
+    {
+        public string Username { get; set; }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var authHelper = new BasicAuthHttpModuleHelper();
+            var authHeader = HttpContext.Current.Request.Headers["Authorization"];
+            try
+            {
+                var authUsername = authHelper.AuthenticateUser(authHeader);
+                if (Username != null && actionContext.ActionArguments.ContainsKey(Username))
+                {
+                    if (authUsername != actionContext.ActionArguments[Username] as string)
+                    {
+                        throw new Exception(string.Format("[{0}] {1}", HttpStatusCode.Unauthorized, "Access denied."));
+                    }
+                }
+                base.OnActionExecuting(actionContext);
+            }
+            catch (Exception ex)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                {
+                    Content = new StringContent(ex.Message)
+            };
+            }
+        }
+    }
+}
